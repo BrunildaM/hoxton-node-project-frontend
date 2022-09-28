a0ef14db4736a8f72e67977f914f94bd712713b1
enesi sefa <enesisefa380@gmail.com>
2022-09-28 11:44:40 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,10 @@ function App() {
   const navigate = useNavigate();
   const [currentUser, setCurrentUser] = useState(null);
   const [users, setUsers] = useState([]);
+  function token(id:number){
+
+
+  }
   function signIn(data: any) {
     fetch(`http://localhost:4000/users`)
       .then((resp) => resp.json())
@@ -32,7 +36,7 @@ function App() {
         <Route index element={<Navigate replace to="/Sign-In" />} />
         {currentUser && <Route path="/chat-page" element={<ChatPage />} />} (
         <>
-          <Route path="/Sign-Up" element={<SignUpPage />} />
+          <Route path="/Sign-Up" element={<SignUpPage signIn={signIn}/>} />
           <Route path="/Sign-In" element={<SignInPage />} />
         </>
         )
--- src/Pages/SignUpPage.tsx
+++ src/Pages/SignUpPage.tsx
@@ -1,19 +1,45 @@
 import "./SignUpPage.css";
 import { Link } from "react-router-dom";
 
-export default function SignUpPage() {
+export default function SignUpPage(signIn: any) {
   return (
     <div className="sign-up-page">
       <h1 className="sign-up-title">Hi App</h1>
-      <form action="" className="sign-up-form">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          fetch("http://localhost:4000/sign-up", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              // @ts-ignore
+              email: e.target.email.value,
+              // @ts-ignore
+              password: e.target.password.value,
+            }),
+          })
+            .then((resp) => resp.json())
+            .then((data) => {
+              // data = {user,token}
+              if (data.error) {
+                alert(data.error);
+              } else {
+                signIn(data);
+              }
+            });
+        }}
+        className="sign-up-form"
+      >
         <h2>Sign Up</h2>
         <label htmlFor="">
           Email:
-          <input type="email" />
+          <input type="email" name="email" />
         </label>
         <label htmlFor="">
           Password:
-          <input type="password" />
+          <input type="password" name="password" />
         </label>
       </form>
       <h3>
